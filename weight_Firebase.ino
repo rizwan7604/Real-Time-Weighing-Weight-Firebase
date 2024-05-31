da56de30bbf81cc949105c4bbf8595efd067b219
Rizwan Shah <154887384+rizwan7604@users.noreply.github.com>
2024-05-31 23:44:46 +0530

    Add files via upload

---
+++ weight_Firebase.ino
@@ -0,0 +1,128 @@
+#include <HX711_ADC.h>
+#if defined(ESP8266) || defined(ESP32)
+#include <EEPROM.h>
+#include <WiFi.h>
+#include <FirebaseESP32.h> // Firebase ESP32 library
+#endif
+
+// Pins
+const int HX711_dout = 23; // MCU > HX711 dout pin
+const int HX711_sck = 22; // MCU > HX711 sck pin
+
+// HX711 constructor
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+// WiFi credentials
+const char* ssid = "TrojanHorse";
+const char* password = "virus.bat";
+
+// Firebase credentials
+#define FIREBASE_HOST "https://real-time-wieght-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define FIREBASE_AUTH "1MwtgVlxpfqUqnudaBXcqT4RVyK7XVQHvBXL96Uv"
+
+// Firebase objects
+FirebaseData firebaseData;
+FirebaseAuth firebaseAuth;
+FirebaseConfig firebaseConfig;
+
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
+
+void setup() {
+  Serial.begin(57600); delay(10);
+  Serial.println();
+  Serial.println("Starting...");
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Connected to Wi-Fi");
+
+  // Configure Firebase
+  firebaseConfig.host = FIREBASE_HOST;
+  firebaseConfig.signer.tokens.legacy_token = FIREBASE_AUTH;
+
+  // Initialize Firebase
+  Firebase.begin(&firebaseConfig, &firebaseAuth);
+  Firebase.reconnectWiFi(true);
+
+  float calibrationValue; // calibration value
+  calibrationValue = 216.83; // uncomment this if you want to set this value in the sketch
+#if defined(ESP8266) || defined(ESP32)
+  //EEPROM.begin(512); // uncomment this if you use ESP8266 and want to fetch this value from eeprom
+#endif
+  //EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch this value from eeprom
+
+  LoadCell.begin();
+  //LoadCell.setReverseOutput();
+  unsigned long stabilizingtime = 2000; // tare precision can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; // set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+  } else {
+    LoadCell.setCalFactor(calibrationValue); // set calibration factor (float)
+    Serial.println("Startup is complete");
+  }
+  while (!LoadCell.update());
+  Serial.print("Calibration value: ");
+  Serial.println(LoadCell.getCalFactor());
+  Serial.print("HX711 measured conversion time ms: ");
+  Serial.println(LoadCell.getConversionTime());
+  Serial.print("HX711 measured sampling rate HZ: ");
+  Serial.println(LoadCell.getSPS());
+  Serial.print("HX711 measured settling time ms: ");
+  Serial.println(LoadCell.getSettlingTime());
+  Serial.println("Note that the settling time may increase significantly if you use delay() in your sketch!");
+  if (LoadCell.getSPS() < 7) {
+    Serial.println("!!Sampling rate is lower than specification, check MCU>HX711 wiring and pin designations");
+  } else if (LoadCell.getSPS() > 100) {
+    Serial.println("!!Sampling rate is higher than specification, check MCU>HX711 wiring and pin designations");
+  }
+}
+
+void loop() {
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 500; // increase value to slow down serial print activity
+
+  // Check for new data/start next conversion
+  if (LoadCell.update()) newDataReady = true;
+
+  // Get smoothed value from the dataset
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      float weight = LoadCell.getData();
+         Serial.print("Load_cell output val: ");
+        Serial.print(weight); // Print weight value
+        Serial.println(" g"); // Print unit (gram)
+      
+      String weightWithUnit = String(weight) + " g";
+      newDataReady = 0;
+      t = millis();
+
+      // Send weight value to Firebase
+      if (Firebase.setString(firebaseData, "/weight", weightWithUnit)) {
+        Serial.println("Data sent to Firebase successfully");
+      } else {
+        Serial.print("Failed to send data to Firebase: ");
+        Serial.println(firebaseData.errorReason());
+      }
+    }
+  }
+
+  // Receive command from serial terminal, send 't' to initiate tare operation
+  if (Serial.available() > 0) {
+    char inByte = Serial.read();
+    if (inByte == 't') LoadCell.tareNoDelay();
+  }
+
+  // Check if last tare operation is complete
+  if (LoadCell.getTareStatus() == true) {
+    Serial.println("Tare complete");
+  }
+}
